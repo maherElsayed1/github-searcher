324f402a4ff2075b7f577b79f7d2d5db510fafe6
Maher Elsayed <maher.elsayed.dev@gmail.com>
2021-02-10 13:59:31 +0200

    feat(state): add action types for the API call

---
+++ src/state/action-types/index.ts
@@ -0,0 +1,5 @@
+export enum ActionType {
+    SEARCH_GITHUB_REQUEST = "search_github_request",
+    SEARCH_GITHUB_SUCCESS = "search_github_success",
+    SEARCH_GITHUB_ERROR = "search_github_error",
+}

709f273a7ebaf4e9713db0f8c5d59260ef95921a
Maher Elsayed <maher.elsayed.dev@gmail.com>
2021-02-10 16:10:36 +0200

    feat: update App component to add routes and redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './state'
+import Home from './pages/Home'
+
 function App() {
-    return <div className="App"></div>
+    return (
+        <div className="App">
+            <Provider store={store}>
+                <Router>
+                    <Switch>
+                        <Route path="/" component={Home} exact />
+                    </Switch>
+                </Router>
+            </Provider>
+        </div>
+    )
 }
 
 export default App

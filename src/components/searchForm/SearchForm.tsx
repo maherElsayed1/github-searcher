f5e1faa3f6839ae1fd69a5ea6af4987c00251664
Maher Elsayed <maher.elsayed.dev@gmail.com>
2021-02-10 23:42:11 +0200

    feat(components): update search form structure & add its functionality
    add the form structure & link it with redux to dispatch the searchGithub action when entering more than 3 letters in the input field

--- src/components/searchForm/SearchForm.tsx
+++ src/components/searchForm/SearchForm.tsx
@@ -1,9 +1,59 @@
+import { useEffect, useState } from 'react'
+import { useTypedSelector } from '../../hooks/useTypedSelector'
+import { useActions } from '../../hooks/useActions'
+
+// icons
+import { AiOutlineGithub } from 'react-icons/ai'
+
+// styles
 import './SearchForm.scss'
 
 const SearchForm: React.FC = () => {
+    const [term, setTerm] = useState('users')
+    const [query, setQuery] = useState('')
+
+    const { searchGithub } = useActions()
+    const { data, error, loading } = useTypedSelector((state) => state.search)
+
+    useEffect(() => {
+        console.log(term, query)
+        if (query.length < 3) {
+            return
+        }
+        searchGithub(term, query)
+    }, [term, query])
+
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        if (query.length < 3) {
+            return alert('please enter at least 3 characters!')
+        }
+    }
+
     return (
-        <div>
-            <h1>Search Component</h1>
+        <div className="container search-page">
+            <div className="search-form">
+                <div className="search-form__header">
+                    <div className="search-form__header-icon">
+                        <AiOutlineGithub />
+                    </div>
+                    <div className="search-form__header-content">
+                        <h1>Github Searcher</h1>
+                        <p>Search Users or repositories below</p>
+                    </div>
+                </div>
+
+                <div className="search-form__content">
+                    <form onSubmit={onSubmit}>
+                        <input value={query} onChange={(e) => setQuery(e.target.value)} placeholder="Start typing to search .." />
+                        <select onChange={(e) => setTerm(e.target.value)}>
+                            <option value="users">Users</option>
+                            <option value="repositories">Repositories</option>
+                            <option value="issues">Issues</option>
+                        </select>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }

e5a298095d50c8e50483fdc4d70256cda51972db
Maher Elsayed <maher.elsayed.dev@gmail.com>
2021-02-11 10:14:18 +0200

    style: add styles for search results

--- src/components/searchResults/SearchResults.tsx
+++ src/components/searchResults/SearchResults.tsx
@@ -1,3 +1,5 @@
+import './SearchResults.scss'
+
 interface SearchResultsProps {
     query: string
     term: string
@@ -10,17 +12,21 @@ const SearchResults: React.FC<SearchResultsProps> = ({ query, term, loading, dat
     return (
         <>
             {!error && !loading && query.length >= 3 && (
-                <div className="search-results">
-                    {term === 'users' &&
-                        data.map((item) => (
-                            <div className="results__item" key={item.id}>
-                                <div className="avatar">
-                                    <img src={item.avatar_url} alt="" />
+                <div className="results-wrapper">
+                    <div className="results">
+                        {term === 'users' &&
+                            data.map((item) => (
+                                <div className="results__item" key={item.id}>
+                                    <div className="results__item-avatar">
+                                        <img src={item.avatar_url} alt={item.login} />
+                                    </div>
+                                    <h3 className="results__item-title">@{item.login}</h3>
+                                    <a href={item.html_url} className="results__item-link" target="_blank" rel="noopener noreferrer">
+                                        Github Profile
+                                    </a>
                                 </div>
-                                <div>{item.login}</div>
-                                <a href={item.html_url}>Github Profile</a>
-                            </div>
-                        ))}
+                            ))}
+                    </div>
                 </div>
             )}
         </>

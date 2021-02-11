8bfe00ba50d5e2c247adc1511f6acb61b7f8ca4d
Maher Elsayed <maher.elsayed.dev@gmail.com>
2021-02-11 19:27:19 +0200

    feat: add repositories results

--- src/components/searchResults/SearchResults.tsx
+++ src/components/searchResults/SearchResults.tsx
@@ -26,6 +26,37 @@ const SearchResults: React.FC<SearchResultsProps> = ({ query, term, loading, dat
                                     </a>
                                 </div>
                             ))}
+
+                        {term === 'repositories' &&
+                            data.map((item) => (
+                                <div className="results__item" key={item.id}>
+                                    <h3 className="results__item-title">{item.full_name}</h3>
+                                    <ul className="results__item-list">
+                                        <li className="results__item-owner">
+                                            <span>owner: </span>
+                                            <a href={item.owner && item.owner.html_url} className="results__item-owner-link" target="_blank" rel="noopener noreferrer">
+                                                <div className="results__item-owner-avatar">{item.owner && <img src={item.owner.avatar_url} alt={item.owner.login} />} </div>
+                                                {item.owner && item.owner.login}
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <span>Description: </span> {item.description}
+                                        </li>
+                                        <li>
+                                            <span>Language: </span> {item.language}
+                                        </li>
+                                        <li>
+                                            <span>Forks: </span> {item.forks}
+                                        </li>
+                                        <li>
+                                            <span>Open Issues: </span> {item.open_issues}
+                                        </li>
+                                    </ul>
+                                    <a href={item.html_url} className="results__item-link" target="_blank" rel="noopener noreferrer">
+                                        Github Repository
+                                    </a>
+                                </div>
+                            ))}
                     </div>
                 </div>
             )}

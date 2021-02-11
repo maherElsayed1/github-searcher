c8c76f2c2a529ed87b92ab6648cd3758c4285c5b
Maher Elsayed <maher.elsayed.dev@gmail.com>
2021-02-11 19:40:43 +0200

    feat: add issues results

--- src/components/searchResults/SearchResults.tsx
+++ src/components/searchResults/SearchResults.tsx
@@ -57,6 +57,40 @@ const SearchResults: React.FC<SearchResultsProps> = ({ query, term, loading, dat
                                     </a>
                                 </div>
                             ))}
+
+                        {term === 'issues' &&
+                            data.map((item) => (
+                                <div className="results__item" key={item.id}>
+                                    <h3 className="results__item-title">{item.title}</h3>
+                                    <ul className="results__item-list">
+                                        <li className="results__item-owner">
+                                            <span>by: </span>
+                                            <a href={item.user && item.user.html_url} className="results__item-owner-link" target="_blank" rel="noopener noreferrer">
+                                                <div className="results__item-owner-avatar">{item.user && <img src={item.user.avatar_url} alt={item.user.login} />} </div>
+                                                {item.user && item.user.login}
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <span>State: </span> {item.state}
+                                        </li>
+                                        <li>
+                                            <span>Comments: </span> {item.comments}
+                                        </li>
+                                        <li>
+                                            <span>Author Association: </span> {item.author_association}
+                                        </li>
+                                        <li>
+                                            <span>Created At: </span> {item.created_at}
+                                        </li>
+                                        <li>
+                                            <span>Updated At: </span> {item.updated_at}
+                                        </li>
+                                    </ul>
+                                    <a href={item.html_url} className="results__item-link" target="_blank" rel="noopener noreferrer">
+                                        Github Issue
+                                    </a>
+                                </div>
+                            ))}
                     </div>
                 </div>
             )}

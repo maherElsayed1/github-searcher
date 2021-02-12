c08a01a40057bb4690f6ba5ffb08300801b041f3
Maher Elsayed <maher.elsayed.dev@gmail.com>
2021-02-12 17:12:45 +0200

    feat: add redux-persist

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './state'
+import { store, persistor } from './state'
+import { PersistGate } from 'redux-persist/integration/react'
 import Home from './pages/Home'
 
 function App() {
     return (
         <div className="App">
             <Provider store={store}>
-                <Router>
-                    <Switch>
-                        <Route path="/" component={Home} exact />
-                    </Switch>
-                </Router>
+                <PersistGate loading={null} persistor={persistor}>
+                    <Router>
+                        <Switch>
+                            <Route path="/" component={Home} exact />
+                        </Switch>
+                    </Router>
+                </PersistGate>
             </Provider>
         </div>
     )
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,6 +1,19 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import reducers from './reducers'
 
-export const store = createStore(reducers, {}, composeWithDevTools(applyMiddleware(thunk)))
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+
+const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(thunk)))
+
+const persistor = persistStore(store)
+
+export { store, persistor }
